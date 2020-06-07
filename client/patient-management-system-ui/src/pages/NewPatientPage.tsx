bf2a0611e6df3d7023a0253477e7638e16b03720
Daryl <daryl@lyraddigital.com>
2020-06-07 21:39:14 +1000

    Added new form logic.

--- client/patient-management-system-ui/src/pages/NewPatientPage.tsx
+++ client/patient-management-system-ui/src/pages/NewPatientPage.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 import Layout from '../components/shared/Layout';
 
 const NewPatientPage = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = (data: any) => console.log(data);
+
     return (
         <Layout pageTitle="New Patient">
-            <p>Hello</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input name="firstName" defaultValue="test" ref={register({ required: true })} />
+                {errors.firstName && <span>First name is required</span>}
+            
+                <input name="lastName" ref={register({ required: true })} />
+                {/* errors will return when field validation fails  */}
+                {errors.lastName && <span>Last name is required</span>}
+            
+                <input type="submit" />
+            </form>
         </Layout>
     );
 }
